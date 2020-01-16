397d8b816982ce009dabd6396401f7dbfa94ad23
anaperezd <apdarriba@gmail.com>
2020-01-16 10:29:34 +0100

    Initial commit

---
+++ src/main/java/com/fillgaps/podcastcatalog/PodcastCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.fillgaps.podcastcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodcastCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fillgaps/podcastcatalog/PodcastCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fillgaps.podcastcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodcastCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
